94748a947a9cc77af7fdcff168295da1db66b27f
tabby42 <irene.theiss@gmx.at>
2017-12-04 11:36:59 +0100

    add js folder

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// for (var i = 1; i <= 100; i++) {
+// 	if (i % 3 === 0 && i % 5 === 0) {
+// 		document.getElementById("fizzbuzz").innerHTML +=
+// 		"fizzbuzz<br>";
+// 	} else if ( i % 5 === 0) {
+// 		document.getElementById("fizzbuzz").innerHTML +=
+// 		"buzz<br>";
+// 	} else if (i % 3 === 0 ) {
+// 		document.getElementById("fizzbuzz").innerHTML +=
+// 		"fizz<br>";
+// 	}
+// 	else {
+// 		document.getElementById("fizzbuzz").innerHTML +=
+// 		i + "<br>";
+// 	}
+// }
+
+//The program should generate every time different 
+//random number, that should be between -5 and 25. 
+//If the temperature is between -5 and 10 print 
+//"The weather is cold" otherwise print 
+//"The weather is moderate".
+var randomNum = Math.floor((Math.random() * 31) - 5);
+console.log(randomNum);
+if (randomNum >= -5 && randomNum <= 10) {
+	document.getElementById("weather").innerHTML =
+	"The weather is cold";
+} else {
+	document.getElementById("weather").innerHTML =
+	"The weather is moderate";
+}
+
+//max function
+var  numbers = [ 1, 25	, -3, 200];
+function maxValue (numArray) {
+	var max = 0;
+	for (var i = 0; i < numArray.length; i++) {
+		if (numArray[i] > max) {
+			max = numArray[i];
+		}
+	}
+	return max;
+}
+document.getElementById("max").innerHTML = maxValue(numbers);
+
+//product
+var num2 = [1, 3, 7, 10, 2];
+function multiply (numArray) {
+	var product = 0;
+	for (var i = 0; i < numArray.length; i++) {
+		product *= numArray[i];
+	}
+	return product;
+}
+
+
+
